5749fc869a9b49b5613f679af54d9f47029f2b8b
Dennis <oba.dennis@gmail.com>
2019-03-20 03:35:13 -0400

    Added pause play features on videos

--- main.js
+++ main.js
@@ -1,20 +1,71 @@
 const $root = $('html, body'); 
 
 
-// Bottom navigation that reveals the nav on hover 
-    const bottomNav = () => {
-        $('#super').hover(function(){
-            console.log('nav hovered')
-            $('.nav-more').addClass('nav-more-up');
-        },
-            function (){
-            $('.nav-more').removeClass('nav-more-up')
-        })
-        $('#super-item a').on('click', function(){
-            $('.nav-more').removeClass('nav-more-up');
-        })
+let vid1 = false;
+let vid2 = false;
+let vid3 = false; 
+// Media query to match css media query then do something
+    const mq = window.matchMedia("(min-width: 533px)");
+    if (mq.matches) {
+        console.log(mq);
+        vid1 = true;
+        vid2 = true;
+        vid3 = true; 
+        $('video').attr('autoplay', true);
+        $('video').attr('loop', true);
+    } else {
 
     }
+// Play video
+    const playVideo = () => {
+        $('#video-1').click(function(){
+            if (vid1 === false) {
+                vid1 = true;
+                $('.v1').get(0).play();
+            } else {
+                vid1 = false;
+                $('.v1').get(0).pause();
+            }
+        })
+        $('#video-2').click(function () {
+            if (vid2 === false) {
+                vid2 = true;
+                $('.v2').get(0).play();
+            } else {
+                vid2 = false;
+                $('.v2').get(0).pause();
+            }
+        })
+        $('#video-3').click(function () {
+            if (vid3 === false) {
+                vid3 = true;
+                $('.v3').get(0).play();
+            } else {
+                vid3 = false;
+                $('.v3').get(0).pause();
+            }
+        })
+    }
+    
+// Bottom navigation that reveals the nav on hover 
+    const bottomNav = () => {
+        let trigger = false
+        $('#super').on('click', function(){
+            console.log('hover', trigger)
+            if (trigger === false) {
+                trigger = true
+                $('.nav-more').addClass('nav-more-up');
+            } else {
+                trigger = false
+                $('.nav-more').removeClass('nav-more-up');
+            }
+        })
+        $('#super-item a').on('click', function(){
+            console.log('clickyyy')
+            $('.nav-more').removeClass('nav-more-up');
+        })
+    }
+
 // Hover state for portfolio pieces
     const navHover = () => {
         $('.nav-port').hover(function() {
@@ -27,7 +78,8 @@ const $root = $('html, body');
             $('.port-nav-item').removeClass('port-nav-item-up');
         })
     }
-// This opens a new tab using jquery
+
+// This opens a new tab to the Live and Github pages
     const radio = () => {
         window.open('https://covox01.github.io/broken-radio-puzzle/');
     }
@@ -57,15 +109,6 @@ const $root = $('html, body');
         })
     }
 
-const init = () => {
-    navHover();
-    goToPortfolio();
-    bottomNav();
-    AOS.init({
-
-    });
-}
-
 const smooth = () => {
     $('a').smoothScroll({
         easing: 'swing',
@@ -77,8 +120,19 @@ const smooth = () => {
     })
 }
 
+const init = () => {
+    navHover();
+    goToPortfolio();
+    bottomNav();
+    playVideo();
+    AOS.init({});
+    smooth();
+}
+
+
+
 $(function(){
     init();
-    smooth();
+    
     
 }); 
